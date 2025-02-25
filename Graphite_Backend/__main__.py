5e6198a64e757c599b9e40fe92a1ddefb6210a57
Kierzyn <joke.and.jest@gmail.com>
2025-02-25 17:41:00 +0100

    Update

--- Graphite_Backend/__main__.py
+++ Graphite_Backend/__main__.py
@@ -9,28 +9,7 @@ def get_processed_data():
 
     df = pd.read_csv("D:\Documents\Analiza\Trening\Dane\Sleep.csv")
 
-    map = {
-        "Yes": 1,
-        "No": 0,
-        "Female": 0,
-        "Male": 1,
-        "Low": 0,
-        "Medium": 1,
-        "High": 2,
-        "Sleep Apnea": 1,
-        "Insomnia": 2,
-        "Normal Weight": 0,
-        "Normal": 0,
-        "Overweight": 1,
-        "Obese": 2,
-    }
-    df_num = df.replace(map)
-    df_num = df_num.fillna(0)
-    df_num[["Systolic", "Diastolic"]] = df_num["Blood Pressure"].str.split(
-        "/", expand=True
-    )
-    df_num = df_num.drop(columns=["Occupation", "Person ID", "Blood Pressure"])
-    df_ss = df_num.groupby(['Sleep Duration', 'Stress Level']).size().reset_index(name='Occurrences')
+    df_ss = df.groupby(['Sleep Duration', 'Stress Level']).size().reset_index(name='Occurrences')
 
     return {
         "data": {
